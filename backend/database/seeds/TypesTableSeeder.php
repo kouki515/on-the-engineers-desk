0cb74a6a24add6a9f0675fc523d80653668412aa
soumu <74505201+soumu129@users.noreply.github.com>
2021-05-03 18:41:38 +0900

    edit add type seeder

---
+++ backend/database/seeds/TypesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('types')->insert([
+            [
+                'type_name' => 'mouse'
+            ],
+            [
+                'type_name' => 'keyborad'
+            ],
+            [
+                'type_name' => 'monitor'
+            ],
+            [
+                'type_name' => 'audio'
+            ],
+            // [
+            //     'type_name' => 'pc'
+            // ],
+            // [
+            //     'type_name' => 'chair'
+            // ],
+        ]);
+    }
+}
